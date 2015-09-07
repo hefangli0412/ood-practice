03b5d0a27e6d974955dd1972f39a0c14a6e5a250
Hefang Li <hefangli0412@users.noreply.github.com>
2015-09-07 12:00:40 -0700

    Update File.java

--- FileSystem/File.java
+++ FileSystem/File.java
@@ -1,80 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+class File extends Entry {
+	private String content;
+	private int size;
 
-class Directory extends Entry {
-	protected List<Entry> contents;
-
-	public Directory(String n, Directory p) {
+	public File(String n, Directory p, int sz) {
 		super(n, p);
-		assert p == this;
-		contents = new ArrayList<Entry>();
+		size = sz;
 	}
 
-	protected List<Entry> getContents() {
-		lastAccessedTime = System.currentTimeMillis();
-		return contents;
-	}
-	
 	public int size() {
-		int size = 0;
-		for (Entry e : contents) {
-			size += e.size();
-		}
 		return size;
 	}
 
-	public int numberOfFiles() {
-		int count = 0;
-		for (Entry e : contents) {
-			if (e instanceof Directory) {
-				count++; // Directory counts as a file
-				Directory d = (Directory) e;
-				count += d.numberOfFiles();
-			} else if (e instanceof File) {
-				count++;
-			}
-		}
-		return count;
+	public String getContents() {
+		lastAccessedTime = System.currentTimeMillis();
+		return content;
 	}
 
-	public boolean deleteEntry(Entry entry) {
+	public void setContents(String c) {
 		lastUpdatedTime = System.currentTimeMillis();
-		return contents.remove(entry);
+		content = c;
 	}
-
-	public void addEntry(Entry entry) {
-		lastUpdatedTime = System.currentTimeMillis();
-		binaryInsert(contents, entry);
-	}
-	
-	/**************************** helper methods ****************************/
-	
-	private void binaryInsert(List<Entry> list, Entry targetEntry) {
-		assert list != null;
-		
-		if (list.size() == 0) {
-			list.add(targetEntry);
-			return;
-		}
-		
-		int left = 0;
-		int right = list.size() - 1;
-		while (left <= right) {
-			int mid = left + (right - left) / 2;
-			int cmp = list.get(mid).name.compareTo(targetEntry.name);
-			if (cmp == 0) {
-				list.add(mid + 1, targetEntry);
-				return;
-			} else if (cmp < 0) {
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		
-		list.add(left, targetEntry);
-	}
-
 }
